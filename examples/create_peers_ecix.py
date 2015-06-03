80e357f104f40b92bc48fff983a16ed88640b82f
Elisa Jasinska <elisa@jasinska.de>
2015-06-03 10:18:16 -0700

    example ecix pull script

---
+++ examples/create_peers_ecix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import json
+import urllib
+
+url = "https://www.ecix.net/memberlist_BER.json"
+response = urllib.urlopen(url)
+ixp_data = json.loads(response.read())
+
+for member in ixp_data['member_list']:
+  for connection in member['connection_list']:
+    for vlan in connection['vlan_list']:
+      if 'ipv4' in vlan:
+        print '-----'
+        print 'neighbor '+vlan['ipv4']['address']+' remote-as '+member['asnum']
+        print 'neighbor '+vlan['ipv4']['address']+' description PEER::'+member['name']
+
