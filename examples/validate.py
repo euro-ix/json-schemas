968406f84a821d38b47361603baad51aba278e3f
Colin Silcock <colin@linx.net>
2016-05-25 16:52:21 +0100

    Add http basic auth support to validate.py (+1 squashed commit) Squashed commits: [bb8835c] Add http basic auth support to validate.py
    Switch to using the requests library for easier handling of http requests
    Switch to using getopt to parse command line arguments

--- examples/validate.py
+++ examples/validate.py
@@ -1,18 +1,62 @@
 #!/usr/bin/env python
 
-import json
-import jsonschema
 import sys
-import urllib
+import argparse
+import requests
+import jsonschema
 
-if len(sys.argv) != 2:
-    raise Exception('usage: validate.py <url>')
 
-url = sys.argv[1]
-schema_url = "https://raw.githubusercontent.com/euro-ix/json-schemas/master/ixp-member-list.schema.json"
+def parse_args(args):
+    schema_url = ('https://raw.githubusercontent.com'
+                  '/euro-ix/json-schemas/master/'
+                  'ixp-member-list.schema.json')
 
-schema = json.loads(urllib.urlopen(schema_url).read())
-ixp_data = json.loads(urllib.urlopen(url).read())
+    parser = argparse.ArgumentParser(
+        description='Validate json output against schema.'
+    )
+    parser.add_argument(
+        '-u',
+        '--username',
+        default=None,
+        help='HTTP Basic Auth User'
+    )
+    parser.add_argument(
+        '-p',
+        '--password',
+        default=None,
+        help='HTTP Basic Auth Password'
+    )
+    parser.add_argument(
+        '-s',
+        '--schema',
+        default=schema_url,
+        help='URL of json schema to validate against'
+    )
+    parser.add_argument('url', help='URL of json to validate')
+    return parser.parse_args(args)
 
-jsonschema.validate(ixp_data, schema)
 
+def main(argv=sys.argv[1:]):
+    args = parse_args(argv)
+
+    schema_url = args.schema
+    response = requests.get(schema_url)
+    schema = response.json()
+
+    if args.username and args.password:
+        response = requests.get(
+            args.url,
+            auth=(
+                args.username,
+                args.password
+            )
+        )
+    else:
+        response = requests.get(args.url)
+
+    ixp_data = response.json()
+    jsonschema.validate(ixp_data, schema)
+
+
+if __name__ == '__main__':
+    main()
